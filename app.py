17b2a5620c8281d2f8f01384e2866b511736abc2
“afadilla13” <128363337+afadilla13@users.noreply.github.com>
2023-11-01 17:52:37 +1100

    adding eda_output and styling

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
+import pandas as pd
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
@@ -18,39 +20,37 @@ def predict():
 
     # Encoding for 'type' feature
     type_encoding = {
-        'type0': 0,
-        'type1': 1,
-        'type2': 2,
-        # Add other encodings if there are more types
+    'House': 0,
+    'Townhouse': 1,
+    'Unit': 2
     }
     encoded_type = type_encoding[data['type']]
 
     # Encoding for 'region' feature
     region_encoding = {
-        'region0': 0,
-        'region1': 1,
-        'region2': 2,
-        'region3': 3,
-        'region4': 4,
-        'region5': 5,
-        'region6': 6,
-        'region7': 7,
-        # Add other encodings if there are more regions
+        'Eastern Metropolitan': 0,
+        'Eastern Victoria': 1,
+        'Northern Metropolitan': 2,
+        'Northern Victoria': 3,
+        'South-Eastern Metropolitan': 4,
+        'Southern Metropolitan': 5,
+        'Western Metropolitan': 6,
+        'Western Victoria': 7,
     }
     encoded_region = region_encoding[data['region']]
 
     # Prepare data for prediction
-    prediction_data = [
-        data['rooms'],
-        encoded_type,
-        data['bathroom'],
-        data['garage'],
-        data['distance'],
-        encoded_region  # Use the encoded value
-    ]
+    prediction_data = pd.DataFrame({
+        'Rooms': [data['rooms']],
+        'Type': [encoded_type],
+        'Distance': [data['distance']],
+        'Bathroom': [data['bathroom']],
+        'Car': [data['garage']],
+        'Region': [encoded_region]
+    })
 
     # Make prediction
-    prediction = model.predict([prediction_data])[0]
+    prediction = model.predict(prediction_data)[0]
     formatted_prediction = "${:,.2f}".format(prediction)
     return jsonify(result=formatted_prediction)
 
